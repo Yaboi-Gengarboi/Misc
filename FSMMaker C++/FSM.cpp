9390647c9403e6177f62f03b9a93ac34864bcd8d
Justyn Durnford <jpdurnford@alaska.edu>
2019-11-09 00:41:29 -0800

    bleh

--- FSMMaker C++/FSM.cpp
+++ FSMMaker C++/FSM.cpp
@@ -2,32 +2,114 @@
 FSM.cpp
 Justyn P. Durnford
 Created on 10/24/2019
-Last updated on
-Source file for FSM struct
+Last updated on 11/9/2019
 */
 
-#include "FSM.hpp"
+#include <iostream>
+#include <string>
 #include <sstream>
+#include <vector>
 
+using std::cin;
+using std::cout;
+using std::endl;
+using std::getline;
 using std::string;
-using std::stringstream;
+using std::istringstream;
+using std::vector;
 
+vector<string> characters =
+{
+	"Captain Falcon",
+	"Bowser"
+};
+
+/*
+
+*/
 struct FSM
 {
 	unsigned short int character;
 	unsigned short int frame;
 	unsigned short int action;
 	float multiplier;
-	std::string toHexString()
-	{
-		string hexStr;
-
-		stringstream stream;
-
-		return hexStr;
-	}
-	std::string toString()
-	{
-
-	}
+	string hexStr;
+	string engStr;
 };
+
+/*
+
+*/
+void printFSMVectorHex(const vector<FSM>& fsmVec)
+{
+	for (FSM fsm : fsmVec)
+		cout << fsm.hexStr << endl;
+}
+
+/*
+
+*/
+void printFSMVectorEng(const vector<FSM>& fsmVec)
+{
+	for (FSM fsm : fsmVec)
+		cout << fsm.engStr << endl;
+}
+
+/*
+
+*/
+string getInput()
+{
+	string str = "";
+	getline(cin, str);
+	return str;
+}
+
+/*
+
+*/
+unsigned short int getCharacter()
+{
+	unsigned short int ch = 0;
+	bool cont = true;
+	string str;
+
+	while (cont)
+	{
+		str = getInput();
+		for (unsigned short i = 0; i < characters.size(); ++i)
+		{
+			if (str.compare(characters[i]) == 0)
+			{
+				ch = i;
+				cont = false;
+			}
+		}
+		if (cont)
+		{
+			cout << "Invalid input. Try again." << endl;
+		}
+	}
+	
+	return ch;
+}
+
+int main()
+{
+	unsigned short int ch;
+	//bool cont = true;
+	//while (cont)
+	//{
+
+	//}
+	
+	cout << "Enter a character. Valid characters include: " << endl;
+	for (string character : characters)
+		cout << character << endl;
+	ch = getCharacter();
+	cout << ch << endl;
+
+	
+
+	return 0;
+}
