d8c477bac666671c68ce442294e0dce1413c8478
Justyn Durnford <jpdurnford@alaska.edu>
2019-12-06 09:29:24 -0800

    started

---
+++ Calculator/Calculator_main.cpp
@@ -0,0 +1,11 @@
+//Calculator_main.cpp
+//Justyn P. Durnford
+//Created on 12/6/2019
+//Last Updated on 12/6/2019
+
+#include "Includer.hpp"
+
+int main()
+{
+
+}
---
+++ Calculator/Includer.hpp
@@ -0,0 +1,38 @@
+//Includer.hpp
+//Justyn P. Durnford
+//Created on 12/5/2019
+//Last Updated on 12/6/2019
+
+#include <FL/Fl.H>
+#include <FL/Fl_Window.H>
+#include <FL/Fl_Output.H>
+#include <FL/Fl_Button.H>
+
+#include <string>
+using std::string;
+using std::stoi;
+using std::stod;
+using std::to_string;
+
+#include <vector>
+using std::vector;
+
+#include <random>
+using std::random_device;
+using std::default_random_engine;
+using std::uniform_int_distribution;
+
+#include <cmath>
+using std::abs;
+using std::log;
+using std::sin;
+using std::cos;
+using std::tan;
+using std::pow;
+using std::asin;
+using std::acos;
+using std::atan2;
+using std::tgamma;
+
+#include <stdexcept>
+using std::invalid_argument;
